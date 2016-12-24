5bde946221e51fb4303e522199e5cdd63079660d
smitchel <smitchel@smitchel-PC>
2016-12-24 15:14:45 -0500

    consolidate tests

--- tests/api_test.js
+++ tests/api_test.js
@@ -100,3 +100,31 @@ suite('API Tests', function () {
 
 });
 
+suite('API Konfig Module Tests', function () {
+    
+    var logToConsole = true;
+   
+    setup(function() {
+        
+    });
+    
+    var base = 'http://localhost:18963';
+    
+    test('should get konfig object of type xls from API', function (done) {
+        var datasource = 'xls';
+
+        rest.get(base + '/api/konfigfactory/' + datasource).on('complete', function (data) {
+            if(logToConsole){console.log(data)};
+            var obj = JSON.parse(data);
+            assert(obj.dataSourceType === datasource );
+            assert(obj.dataSource.dataBricks[0].sheet === '' );
+            done();
+
+        });
+
+    });
+    
+    
+    
+    
+    });
